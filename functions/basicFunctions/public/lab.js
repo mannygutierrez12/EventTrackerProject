6358f94d33067f739fb400ae34ef8e5b956a0869
mannygutierrez12 <iberiard@gmail.com>
2023-06-06 15:05:30 -0400

    Rob's update

---
+++ functions/basicFunctions/public/lab.js
@@ -0,0 +1,55 @@
+/*
+1 : Write a function named 'reverse' ...  
+  
+This function will take one argument, a string.  
+  
+Return the submitted string with the characters in reverse order.  
+  
+***NOTE*** Do not use the 'reverse' method on the String object, write your own.
+*/
+
+/************** Your Solution Here ****************/
+
+/**************************************************/
+
+/*
+2 : Write a function named 'palindromeChecker' ...  
+  
+This function will take one argument, a string.  
+  
+If the provided string is a palindrome (the same backwards and forwards), return
+true, otherwise return false.
+*/
+
+/************** Your Solution Here ****************/
+
+/**************************************************/
+
+/*
+3 : Write a function named 'isPrime' ...  
+  
+This function will take one argument, a whole number.  
+  
+If the provided number is prime, return true, otherwise, return false.
+*/
+
+/************** Your Solution Here ****************/
+
+/**************************************************/
+
+/* 
+4 : Write a function named 'computeTax'. 
+
+This function will take one or two arguments.  
+  
+* The first argument will always be the amount tendered. 
+
+* The second argument, which is optional, could be the tax percentage (as a 
+whole number). If no second argument is provided, assume the tax rate is 7.5%.  
+  
+Once applying the correct amount of tax, return the amount of tax owed.  
+*/
+
+/************** Your Solution Here ****************/
+
+/**************************************************/
